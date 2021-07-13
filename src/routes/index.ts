253ec68ada5a56e347e73440f65b6eacfe2a2198
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-07-13 20:49:46 +0100

    added admin controls and user updating

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,13 @@ const router = Router();
 import subreddit from './subreddit';
 import post from './post';
 import comment from './comment';
+import user from './user';
 
 router.get('/', (_req, res) => res.send('wtf get off'));
 
 router.use('/subreddit', subreddit);
 router.use('/post', post);
 router.use('/comment', comment);
+router.use('/user', user);
 
 export default router;
--- src/routes/subreddit/index.ts
+++ src/routes/subreddit/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 const router = Router();
 
 import createSubreddit from './create';
+import admin from './admin';
 
 router.get('/', (req, res) => res.send('test'));
 router.use('/createSubreddit', createSubreddit);
+router.use('/admin', admin);
 
 export default router;
