e093a66b9ed3d09cbc0b1be30bbb05324fb919fc
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-07-13 18:43:57 +0100

    attempting to change port

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,6 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-const server = app.listen(5000, () => {
-	console.log('Server listening on port 5000');
+const server = app.listen(3000, () => {
+	console.log('Server listening on port 3000');
 });
