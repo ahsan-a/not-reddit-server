28bcdd6c7ec6c48113969be8d8896931c479862a
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-07-14 18:33:32 +0100

    added rate limiting and removed ts-node

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 import routes from './routes';
 
@@ -16,8 +17,17 @@ app.use(
 );
 
 app.use(morgan('dev'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// rate limiting
+app.set('trust proxy', 1);
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 10, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 app.use(routes);
 
